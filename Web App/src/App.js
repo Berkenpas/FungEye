5e2298bbef7ba6b1e464ae21c5532cc9bcddc548
vickaykikay <83044235+vickaykikay@users.noreply.github.com>
2021-10-16 20:50:45 -0700

    Add files via upload

--- Web App/src/App.js
+++ Web App/src/App.js
@@ -1,1 +1,24 @@
+import logo from './mushcute.gif';
+import './App.css';
 
+function uploadImage(e){
+
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Please upload an image.
+        </p>
+        <button className = "upload-button" onClick={uploadImage}>
+          Upload
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
