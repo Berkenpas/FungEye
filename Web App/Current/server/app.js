0bf36fe3911d065e36a30a4880c356f773bfa9e2
vickaykikay <83044235+vickaykikay@users.noreply.github.com>
2021-12-07 13:04:43 -0800

    Add files via upload

---
+++ Web App/Current/server/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+const mongoose = require('mongoose');
+const {MONGOURI} = require('./keys');
+const cors = require('cors');
+
+
+mongoose.connect(MONGOURI, {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+mongoose.connection.on('connected', ()=>{
+    console.log('connected to mongo');
+})
+mongoose.connection.on('error', (err)=>{
+    console.log('connection error', err);
+})
+
+require ('./models/user');
+require('./models/post');
+mongoose.model("User");
+
+app.use(express.json());
+app.use(require('./routes/auth'));
+app.use(require('./routes/post'));
+
+app.listen(PORT, ()=>{
+    console.log('server is running on ', PORT);
+})
---
+++ Web App/Current/server/keys.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI: 'mongodb+srv://test:test@cluster0.ra83u.mongodb.net/InstaClone?retryWrites=true&w=majority',
+    JWT_SECRET: 'CaliforniaSecret37'
+}
