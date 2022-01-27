6853fc671cbfdff327c494461de01e528f7b5ceb
vickaykikay <83044235+vickaykikay@users.noreply.github.com>
2022-01-26 19:22:05 -0800

    Add files via upload

---
+++ Web App/Current.v.1.26.2022/client/src/App.js
@@ -0,0 +1,57 @@
+import React, {useEffect, createContext, useReducer, useContext} from 'react';
+import NavBar from './components/navbar'
+import "./App.css"
+import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom';
+import Home from './components/screens/Home'
+import Profile from './components/screens/Profile'
+import Login from './components/screens/Login'
+import Signup from './components/screens/Signup'
+import CreatePost from './components/screens/CreatePost'
+import {reducer, initialState} from './reducers/userReducer'
+
+export const UserContext = createContext()
+
+const Routing = () =>{
+  const navigate = useNavigate()
+  const{state, dispatch} = useContext(UserContext)
+
+  useEffect(()=>{
+    const user = JSON.parse(localStorage.getItem("user"))    
+    if(user){
+      dispatch({type: "USER", payload: user})
+      //navigate('/')
+    }
+    else{
+      navigate('/login')
+    }
+  }, [])
+
+  return(
+    <Routes>
+        <Route path = '/' element = {<Home/>}/>
+        <Route path = '/login' element = {<Login/>}/>
+        <Route path = '/signup' element = {<Signup/>}/>
+        <Route path = '/myuploads' element = {<Profile/>}/>
+        <Route path = '/uploadimage' element = {<CreatePost/>}/>
+    </Routes>
+     
+  )
+}
+
+
+function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+  return ( 
+    <UserContext.Provider value = {{state, dispatch}}>
+      <BrowserRouter>
+        <NavBar/>
+        <Routing/>
+      </BrowserRouter>
+    </UserContext.Provider>
+    
+
+  );
+}
+
+export default App;
