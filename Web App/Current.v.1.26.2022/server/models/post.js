77dca5ccbbb1bbc53c268ad3641a85ed33cc22e9
berkenpas <berkenpasw@gmail.com>
2022-03-17 23:20:15 -0700

    Got the results page working! It doesn't display what the voted type is, but it now only shows posts that have been voted. in addition, the post schema has been updated to at the very least have the rest of the params.. bdesides ID, which I was unsure how to add

--- Web App/Current.v.1.26.2022/server/models/post.js
+++ Web App/Current.v.1.26.2022/server/models/post.js
@@ -13,6 +13,16 @@ const postSchema = new mongoose.Schema({
     mushID:{
         type: ObjectId,
         ref:"Mushrooms"
+    },
+    voted: {
+        type: Boolean,
+        default: false,
+    },
+    date: {
+        type: Date, //Might need to have extra specifications, required?
+    },
+    __v: {
+        type: int, //not actually sure what __v is... lol
     }
 
 })
